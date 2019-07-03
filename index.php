7934da4626d83ab0bfd5e303af67b4610a5f27d7
Bayusyahrian <bayusyahrianxrpla@gmail.com>
2019-07-03 08:38:41 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
         <title>Coba Git Remote</title>
     </head>
     <body>
-        
+        <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat provident obcaecati, placeat nihil, molestiae optio, odio ex at quis quam consequuntur itaque ipsum commodi quibusdam non ullam culpa quia laboriosam!</p>
     </body>
 </html>
